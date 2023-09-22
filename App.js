b832fc11d687c3d769af9101e82a682fb6e77e3e
Greg Carpenter <greg@boopsie.com>
2023-09-21 19:36:39 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const apiKey = "api_e138069b91e24cbd81329394de4b5c6506605ad690d340cbbf96931d1ff16b66";
+const qranswers = require('qranswers')(apiKey);
+
 export default function App() {
+  useEffect(() => {
+    async function initQR() {
+      const initOk = await qranswers.subscriptions.initialize();
+      if (initOk) {
+        const sub = qranswers.subscriptions.subscribeToResponse((response) => {
+          console.log('response', response);
+        })
+        return sub;
+      }
+    }
+
+    const sub = initQR();
+
+    return () => {
+      qranswers.subscriptions.unsubscribeToResponse(sub);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>

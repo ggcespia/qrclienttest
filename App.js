e79141cf1ef11abd25ff59ab2e48cf87271d1ba3
Greg Carpenter <greg@boopsie.com>
2023-09-29 13:09:31 -0700

    1st test qranswers.api.getProjectList works!

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 //Get your API key from the QR-Answers app
 const apiKey = "api_e138069b91e24cbd81329394de4b5c6506605ad690d340cbbf96931d1ff16b66";
-const qranswers = require('qranswers')(apiKey);
+const qranswers = require("qranswers")(apiKey);
 
 export default function App() {
   useEffect(() => {
     async function initQR() {
+      const initApiOk = await qranswers.api.initialize();      
+      
       const initOk = await qranswers.subscriptions.initialize();
       if (initOk) {
-        const sub = qranswers.subscriptions.subscribeToResponse((response) => {
+        const sub = qranswers.subscriptions.subscribeToAllResponses((response) => {
           console.log('response', response);
         })
         return sub;
@@ -21,13 +23,23 @@ export default function App() {
     const sub = initQR();
 
     return () => {
-      qranswers.subscriptions.unsubscribeToResponse(sub);
+      qranswers.subscriptions.unsubscribeToAllResponses(sub);
     }
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>QR-Answers API Tester</Text>
+      <Pressable onPress={async () => {
+        qranswers.api.getProjectList().then((projects) => {
+          // returns {success: 'success', data: [...]} or {error: 'error message'}
+          if (projects.data) {
+            console.log(projects.data);
+          }
+        })
+      }}>
+        <Text>Get Projects</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
